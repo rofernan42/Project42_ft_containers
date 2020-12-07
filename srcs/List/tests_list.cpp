b1a41d65d0ce18df800120c6546bc5efdc23d23f
user42 <rofernan@student.42.fr>
2020-12-07 14:16:12 +0100

    finish list

--- srcs/List/tests_list.cpp
+++ srcs/List/tests_list.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include "List.hpp"
 
+struct is_near {
+bool	operator() (int first, int second)
+{
+	return (abs(first - second) < 5);
+}
+};
+
+bool	comp(int first, int second)
+{
+	return (first > second);
+}
+
+struct is_odd {
+bool	operator() (const int& value) {
+	return (value % 2) == 1;
+}
+};
+
 template <class T>
 void	print_cap(ft::list<T> lst)
 {
@@ -24,6 +42,15 @@ void	print_access(ft::list<T> lst)
 {
 	std::cout << "lst.front():\t" << lst.front() << std::endl;
 	std::cout << "lst.back():\t" << lst.back() << std::endl;
+	std::cout << std::endl;
+}
+
+template <class T>
+void	print_reverse(ft::list<T> lst)
+{
+	std::cout << "print content with reverse iterarors:\n";
+	for (typename ft::list<T>::reverse_iterator it = lst.rbegin(); it != lst.rend(); it++)
+		std::cout << "*it:\t" << *it << std::endl;
 	std::cout << std::endl;
 }
 
@@ -173,6 +200,206 @@ int main(void)
 	std::cout << "return value of erase lst2 begin to end: " << *lst2.erase(lst2.begin(), lst2.end()) << std::endl;
 	print_cap(lst2);
 	print_content(lst2);
+
+
+
+	/* OPERATIONS */
+
+	std::cout << "\n\n\n******************** TESTS OPERATIONS ********************\n";
+
+	for (int i = 0; i < 10; i++)
+		lst.push_back(i + 1);
+	for (int i = 15; i > 0; i--)
+		lst2.push_front(i);
+
+	std::cout << "********** LST **********\n";
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** LST2 **********\n";
+	print_cap(lst2);
+	print_content(lst2);
+
+	std::cout << "********** SPLICE 1 value **********\n";
+	it = lst.begin();
+	for (int i = 0; i < 4; i++)
+		it++;
+	std::cout << "it: " << *it << std::endl;
+	it2 = lst2.begin();
+	for (int i = 0; i < 12; i++)
+		it2++;
+	std::cout << "it2: " << *it2 << std::endl;
+	lst.splice(it, lst2, it2);
+
+	std::cout << "********** LST **********\n";
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** LST2 **********\n";
+	print_cap(lst2);
+	print_content(lst2);
+
+	std::cout << "********** SPLICE 5 values **********\n";
+	it = lst2.begin();
+	for (int i = 0; i < 7; i++)
+		it++;
+	std::cout << "it: " << *it << std::endl;
+	it2 = lst2.begin();
+	for (int i = 0; i < 12; i++)
+		it2++;
+	std::cout << "it2: " << *it2 << std::endl;
+	lst.splice(lst.end(), lst2, it, it2);
+
+	std::cout << "********** LST **********\n";
+	print_cap(lst);
+	print_content(lst);
+
+	ft::list<int>::reverse_iterator rit = lst.rbegin();
+	std::cout << "rit: " << *rit << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "********** LST2 **********\n";
+	print_cap(lst2);
+	print_content(lst2);
+
+	std::cout << "********** REMOVE 8s FROM LST **********\n";
+	lst.remove(8);
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** REMOVE 10s FROM LST **********\n";
+	lst.remove(10);
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** REMOVE 12s FROM LST **********\n";
+	lst.remove(12);
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** REMOVE 1s FROM LST **********\n";
+	lst.remove(1);
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** REVERSE LST **********\n";
+	lst.reverse();
+	print_cap(lst);
+	print_content(lst);
+
+	lst.push_back(2);
+	lst.push_back(2);
+	lst.push_back(2);
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** UNIQUE LST **********\n";
+	lst.unique();
+	print_cap(lst);
+	print_content(lst);
+
+	lst.push_back(2);
+	lst.push_back(2);
+	lst.push_back(2);
+	lst.push_back(5);
+	lst.push_back(6);
+	lst.push_back(911);
+	lst.push_back(1);
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** UNIQUE LST TEST 2 **********\n";
+	lst.unique();
+	print_cap(lst);
+	print_content(lst);
+
+	lst.push_front(11);
+	lst.push_front(11);
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** UNIQUE LST TEST 3 **********\n";
+	lst.unique();
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** UNIQUE LST PREDICATE **********\n";
+	lst.unique(is_near());
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** SORT LST **********\n";
+	lst.sort();
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** SORT LST comp **********\n";
+	lst.sort(comp);
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** REVERSE LST **********\n";
+	lst.reverse();
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** MERGE LST LST2 **********\n";
+	lst.merge(lst2);
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** LST2 **********\n";
+	print_cap(lst2);
+	print_content(lst2);
+
+	std::cout << "********** LST2 ASSIGN(6, 111) **********\n";
+	lst2.assign(6, 111);
+	print_cap(lst2);
+	print_content(lst2);
+
+	std::cout << "********** MERGE LST LST2 comp **********\n";
+	lst.merge(lst2, comp);
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** SORT LST **********\n";
+	lst.sort();
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** LST2 ASSIGN(3, 222) **********\n";
+	lst2.assign(3, 222);
+	print_cap(lst2);
+	print_content(lst2);
+
+	std::cout << "********** REVERSE LST **********\n";
+	lst.reverse();
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** MERGE LST LST2 comp **********\n";
+	lst.merge(lst2, comp);
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** SORT EMPTY LST2 **********\n";
+	lst2.sort();
+	print_cap(lst2);
+	print_content(lst2);
+
+	std::cout << "********** LST UNIQUE **********\n";
+	lst.unique();
+	print_cap(lst);
+	print_content(lst);
+
+	std::cout << "********** PRINT REVERSE ITERATOR LST **********\n";
+	print_cap(lst);
+	print_reverse(lst);
+
+	std::cout << "********** LST REMOVE IF ODD **********\n";
+	lst.remove_if(is_odd());
+	print_cap(lst);
+	print_content(lst);
 
 	return (0);
 }
